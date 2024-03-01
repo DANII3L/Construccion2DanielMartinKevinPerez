787c1676c70cdde9540a8392ad5deb9777e9d259
DANII3L <bedomartin21@gmail.com>
2024-02-29 23:31:27 -0500

    Inicio

---
+++ src/main/java/app/models/Person.java
@@ -0,0 +1,49 @@
+package app.models;
+
+public class Person {
+	private long id;
+	private String fullName;
+	private String rol;
+	private String username;
+	private String password;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
