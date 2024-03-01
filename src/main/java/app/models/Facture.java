13af03955a9deb8ca31ceadcc39e206be4ba7812
DANII3L <bedomartin21@gmail.com>
2024-03-01 00:48:40 -0500

    Primera parte desarrollo martin

--- src/main/java/app/models/Facture.java
+++ src/main/java/app/models/Facture.java
@@ -1,5 +1,78 @@
 package app.models;
 
-public class Facture {
+import java.util.Date;
 
+public class Facture {
+	private String id;
+	private String petId;
+	private long ownerId;
+	private String orderId;
+	private String medicineName;
+	private double cost;
+	private int amount;
+	private Date date;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPetId() {
+		return petId;
+	}
+
+	public void setPetId(String petId) {
+		this.petId = petId;
+	}
+
+	public long getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(long ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getMedicineName() {
+		return medicineName;
+	}
+
+	public void setMedicineName(String medicineName) {
+		this.medicineName = medicineName;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
--- src/main/java/app/models/Order.java
+++ src/main/java/app/models/Order.java
@@ -1,5 +1,60 @@
 package app.models;
 
-public class Order {
+import java.util.Date;
 
+public class Order {
+	private String id;
+	private String petId;
+	private long ownerID;
+	private long vetId;
+	private String medicineName;
+	private Date dateRegister;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPetId() {
+		return petId;
+	}
+
+	public void setPetId(String petId) {
+		this.petId = petId;
+	}
+
+	public long getOwnerID() {
+		return ownerID;
+	}
+
+	public void setOwnerID(long ownerID) {
+		this.ownerID = ownerID;
+	}
+
+	public long getVetId() {
+		return vetId;
+	}
+
+	public void setVetId(long vetId) {
+		this.vetId = vetId;
+	}
+
+	public String getMedicineName() {
+		return medicineName;
+	}
+
+	public void setMedicineName(String medicineName) {
+		this.medicineName = medicineName;
+	}
+
+	public Date getDateRegister() {
+		return dateRegister;
+	}
+
+	public void setDateRegister(Date dateRegister) {
+		this.dateRegister = dateRegister;
+	}
 }
